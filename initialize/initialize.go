d8a794cf7f1c8df325bef6bdf884ef22d8304b1d
cclulu <cclulu11@gmail.com>
2019-02-17 12:55:46 -0700

    load images once

--- initialize/initialize.go
+++ initialize/initialize.go
@@ -16,54 +16,14 @@ const (
 )
 
 var (
-	imageGameBG   *ebiten.Image
-	imageWindows  *ebiten.Image
-	imageGameover *ebiten.Image
-	lightGray     ebiten.ColorM
+	imageGameBG    *ebiten.Image
+	imageWindows   *ebiten.Image
+	imageGameover  *ebiten.Image
+	imageCharacter *ebiten.Image
+	lightGray      ebiten.ColorM
 )
 
-type Character struct {
-	still *ebiten.Image
-}
-
-type Environment struct {
-}
-
-type Entities struct {
-}
-
-type Window struct {
-	imageGameBG   *ebiten.Image
-	imageWindows  *ebiten.Image
-	imageGameover *ebiten.Image
-}
-
-type ScreenManager struct {
-	Screen *ebiten.Image
-}
-
 func LoadImages() *ebiten.Image {
-	background, err := os.Open("resources/dungeon.jpg")
-	if err != nil {
-		panic("on reading the file")
-	}
-	defer background.Close()
-
-	img, _, err := image.Decode(background)
-	if err != nil {
-		panic(err)
-	}
-
-	imageWindows, err = ebiten.NewImage(ScreenWidth, ScreenHeight, ebiten.FilterDefault)
-	if err != nil {
-		spew.Dump(err)
-	}
-
-	imageGameBG, err = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
-	if err != nil {
-		spew.Dump(err)
-	}
-
 	imageWindows.Fill(color.White)
 
 	w, h := imageGameBG.Size()
@@ -85,4 +45,43 @@ func LoadImages() *ebiten.Image {
 	imageWindows.DrawImage(imageGameBG, op)
 	return imageWindows
 	// screen.DrawImage(imageWindows, &ebiten.DrawImageOptions{})
+}
+
+func init() {
+	background, err := os.Open("resources/dungeon.jpg")
+	if err != nil {
+		panic("on reading the file")
+	}
+	defer background.Close()
+
+	img, _, err := image.Decode(background)
+	if err != nil {
+		panic(err)
+	}
+
+	imageWindows, err = ebiten.NewImage(ScreenWidth, ScreenHeight, ebiten.FilterDefault)
+	if err != nil {
+		spew.Dump(err)
+	}
+
+	imageGameBG, err = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
+	if err != nil {
+		spew.Dump(err)
+	}
+
+	characterSpriteSheet, err := os.Open("resources/MainGuySpriteSheet.png")
+	if err != nil {
+		panic("on reading the file")
+	}
+	defer characterSpriteSheet.Close()
+
+	img, _, err = image.Decode(characterSpriteSheet)
+	if err != nil {
+		panic(err)
+	}
+
+	imageCharacter, err = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
+	if err != nil {
+		spew.Dump(err)
+	}
 }
